f10044e6027400065554736945fda6388e76c07e
jmgarzo <jose-maria.garzo@soprasteria.com>
2022-10-31 11:11:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/blogappjm/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.blogappjm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
