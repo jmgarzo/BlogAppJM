c3e9d9cf5df009a007502d79770f1ae8ecf580fd
jmgarzo-developer <jmgarzo.developer@gmail.com>
2022-11-09 13:14:03 +0100

    Chapter 109.Menu de navegación

--- app/src/main/java/com/example/blogappjm/MainActivity.kt
+++ app/src/main/java/com/example/blogappjm/MainActivity.kt
@@ -2,12 +2,44 @@ package com.example.blogappjm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.blogappjm.core.hide
+import com.example.blogappjm.core.show
+import com.example.blogappjm.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        ObserveDestinationChange()
+    }
+
+    private fun ObserveDestinationChange() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/blogappjm/ui/camera/CameraFragment.kt
@@ -0,0 +1,14 @@
+package com.example.blogappjm.ui.camera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.blogappjm.R
+
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+
+}
